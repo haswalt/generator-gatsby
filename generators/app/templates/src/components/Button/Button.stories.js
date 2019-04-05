e06b0ebf8c3ef1a81922613aed2d7accff904417
Sean King <sean@seanking.org>
2019-04-05 17:20:53 +1300

    Remove href from button stories

--- generators/app/templates/src/components/Button/Button.stories.js
+++ generators/app/templates/src/components/Button/Button.stories.js
@@ -6,7 +6,6 @@ import Button from './index';
 const DESCRIPTION = `Button with various themes`,
   DEFAULT_PROPS = {
     text: 'Click me',
-    href: '',
     theme: 'primary'
   },
   THEME_OPTIONS = {
@@ -19,10 +18,7 @@ storiesOf('Button', module)
     info: DESCRIPTION
   })
   .add('Primary', () => (
-    <Button
-      href={text('Href', DEFAULT_PROPS.href)}
-      theme={select('Theme', THEME_OPTIONS, DEFAULT_PROPS.theme)}
-    >
+    <Button theme={select('Theme', THEME_OPTIONS, DEFAULT_PROPS.theme)}>
       {text('Label', DEFAULT_PROPS.text)}
     </Button>
   ));
