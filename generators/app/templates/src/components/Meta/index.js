743ec3c65328034063ac79a94cfc57ea3ca85dc0
Sean King <sean@seanking.org>
2019-10-16 15:11:23 +0100

    Resolve full URL for social cover images

--- generators/app/templates/src/components/Meta/index.js
+++ generators/app/templates/src/components/Meta/index.js
@@ -1,6 +1,7 @@
 import { Location } from '@reach/router';
 import React from 'react';
 import Helmet from 'react-helmet';
+import resolveRelative from 'resolve-relative-url';
 
 /**
  * Head component
@@ -10,6 +11,8 @@ import Helmet from 'react-helmet';
  * @property {string} cover URL of page cover image (social media)
  */
 export default function Meta({ title = '', description = '', cover = '' }) {
+  const resolvedCover = resolveRelative(cover, '<%= props.url %>');
+
   return (
     <Location>
       {({ location }) => (
@@ -17,27 +20,27 @@ export default function Meta({ title = '', description = '', cover = '' }) {
           {/* Basic */}
           <title>{title}</title>
           <meta name="description" content={description} />
-          <meta name="image" content={cover} />
+          <meta name="image" content={resolvedCover} />
           <link rel="alternate" hreflang="en" href={location.href} />
           <link rel="canonical" href={location.href} />
 
           {/* Schema.org */}
           <meta itemprop="title" content={title} />
           <meta itemprop="description" content={description} />
-          <meta itemprop="image" content={cover} />
+          <meta itemprop="image" content={resolvedCover} />
 
           {/* Facebook */}
           <meta property="og:title" content={title} />
           <meta property="og:site_name" content="<%= props.name %>" />
           <meta property="og:url" content={location.href} />
           <meta property="og:description" content={description} />
-          <meta property="og:image" content={cover} />
+          <meta property="og:image" content={resolvedCover} />
 
           {/* Twitter */}
           <meta property="twitter:card" content="summary" />
           <meta property="twitter:title" content={title} />
           <meta property="twitter:description" content={description} />
-          <meta property="twitter:image" content={cover} />
+          <meta property="twitter:image" content={resolvedCover} />
         </Helmet>
       )}
     </Location>
